86376fa143a0b600f4383555d838d2e5e8be1f5c
JacobSeatlholo <66915934+JacobSeatlholo@users.noreply.github.com>
2021-03-05 10:26:03 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,10 @@
     <div>
       <logo />
       <h1 class="title">
-        nuxtjs
+        Business Hustle
       </h1>
       <h2 class="subtitle">
-        My astonishing Nuxt.js project
+        
       </h2>
       <div class="links">
         <a
@@ -14,14 +14,14 @@
           target="_blank"
           class="button--green"
         >
-          Documentation
+          Home
         </a>
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
           class="button--grey"
         >
-          GitHub
+         Resources
         </a>
       </div>
     </div>
